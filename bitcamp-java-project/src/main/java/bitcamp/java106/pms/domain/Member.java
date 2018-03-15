3e22674cdb8af5470462947826d8e9a11e95f013
Do Gyeong Seo <dgseo8981@gmail.com>
2018-03-15 11:14:41 +0900

    20180315

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/domain/Member.java
+++
@@ -1,7 +0,0 @@
-package bitcamp.java106.pms.domain;
-
-public class Member {
-    public String memID;
-    public String memEM;
-    public String memPW;
-}
--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/domain/Team.java
+++
@@ -1,9 +0,0 @@
-package bitcamp.java106.pms.domain;
-
-public class Team {
-    public String teamName;
-    public String teamDescription;
-    public String maxQty;
-    public String startDate;
-    public String endDate;
-}
